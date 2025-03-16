5c44007fcfee3b06d3367fa7f6274006c97746fc
Nguyen Trong Kha <trongkha08022k4@gmail.com>
2025-03-16 14:28:14 +0700

    add websocket to send data to fe

--- src/main/java/com/web/yolofarm/service/MqttService.java
+++ src/main/java/com/web/yolofarm/service/MqttService.java
@@ -5,6 +5,8 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttSecurityException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.web.yolofarm.component.AdafruitConnection;
@@ -14,9 +16,11 @@ import com.web.yolofarm.configuration.MqttProperties;
 public class MqttService {
     private MqttClient CLIENT;
     private final String TOPIC = "khanguyentrong2k4/feeds/";
+    private final SimpMessagingTemplate messagingTemplate;
 
-    public MqttService(MqttProperties properties) throws MqttSecurityException, MqttException {
+    public MqttService(MqttProperties properties, SimpMessagingTemplate messagingTemplate) throws MqttSecurityException, MqttException {
         CLIENT = AdafruitConnection.getInstance(properties);
+        this.messagingTemplate = messagingTemplate;
 
         // Đảm bảo client đã kết nối trước khi subscribe
         if (!CLIENT.isConnected()) {
@@ -32,6 +36,8 @@ public class MqttService {
                 String payload = new String(message.getPayload());
                 System.out.println("Nhận dữ liệu từ " + t + ": " + payload);
 
+                // Gửi dữ liệu đến client qua websocket
+                messagingTemplate.convertAndSend("/topic/microbit/"+feed, payload);
                 // TODO: Xử lý dữ liệu tại đây (lưu vào DB, hiển thị UI, ...)
             });
         } catch (MqttException e) {
